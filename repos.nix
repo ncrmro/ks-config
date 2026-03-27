ae736e13ea3d205a11989ac1d896b5a191f3acf6
Nicholas Romero <ncrmro@gmail.com>
2026-03-27 10:49:47 -0500

    fix(nix): register nixos-config repo

--- repos.nix
+++ repos.nix
@@ -1,4 +1,7 @@
 {
+  "ncrmro/nixos-config" = {
+    url = "git@github.com:ncrmro/nixos-config.git";
+  };
   "ncrmro/keystone" = {
     url = "git@github.com:ncrmro/keystone.git";
     flakeInput = "keystone";
